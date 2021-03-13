c8d3ef445c2ab6c47ce8d84962d8063667731d81
zciklacekic <zciklacekic@gmail.com>
2021-03-13 22:40:56 +0300

    13 Mart 2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 //React için axios,fetch ile @angular daki httpclient ın aynı işlemini yapıyor
@@ -13,25 +12,17 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded=false;
   apiUrl = 'https://localhost:44350/api/products/getall';
-  // productResponseModel: ProductResponseModel = {
-  //   data: this.products,
-  //   message: '',
-  //   success: true,
-  // };
+
   constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
-    // console.log('Init Calisti');
     this.getProducts();
   }
 
   getProducts() {
-    // console.log("Api REquest Basladi")
     this.productService.getProducts().subscribe(response=>{
       this.products=response.data
       this.dataLoaded=true;
     })
-    // console.log("Request Bitti");
   }
-  // console.log("Metod Bitti");
 }
