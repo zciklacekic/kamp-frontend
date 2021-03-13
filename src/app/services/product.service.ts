c69776858176e0694b597d2f6baeccf373f6d4d8
zciklacekic <zciklacekic@gmail.com>
2021-03-14 00:00:06 +0300

    13 Mart DersSonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,10 +9,15 @@ import { Product } from '../models/product';
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl = 'https://localhost:44350/api/products/getall';
+  apiUrl = 'https://localhost:44350/api/';
   constructor(private httpClient: HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath= this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath= this.apiUrl + "products/getbycategory?categoryId=" + categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
