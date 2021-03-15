2e6009aadfa4f75e299caf8467f65e4d122c20d6
zciklacekic <zciklacekic@gmail.com>
2021-03-15 12:36:50 +0300

    Category Tum urunler eklendi

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,35 +5,44 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-  categories:Category[]=[];
-  currentCategory:Category;
-  dataLoaded=false;
+  categories: Category[] = [];
+  currentCategory: Category |null;
+  dataLoaded = false;
   apiUrl = 'https://localhost:44350/api/categories/getall';
 
-  constructor(private categoryService:CategoryService) {}
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.getCategories();
   }
 
   getCategories() {
-    this.categoryService.getCategories().subscribe(response=>{
-      this.categories=response.data
-      this.dataLoaded=true;
-    })
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
   }
-  setCurrentCategory(category:Category){
-      this.currentCategory=category;
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
   }
-  getCurrentCategoryClass(category:Category){
-    if(category ==this.currentCategory){
-      return "list-group-item active"
-    }else{
-      return "list-group-item"
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
   }
-
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+  cleanCurrentCategory() {
+    this.currentCategory = null;
+  }
 }
